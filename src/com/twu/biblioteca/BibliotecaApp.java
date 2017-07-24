491d027ca412a1cdbd0d747cc2d893fdcf6f6a03
XluoChen <cxluo@thoughtworks.com>
2017-07-24 21:26:52 +0800

    realize method to list books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,14 +1,28 @@
 package com.twu.biblioteca;
 
 public class BibliotecaApp {
+    Book[] books = new Book[5];
+
+    BibliotecaApp() {
+        for (int i = 0;i < this.books.length;i++) {
+            this.books[i] = new Book("《linux指南》", "David", "人民邮电出版社");
+        }
+    }
 
     String printWelcomeMessage(String message) {
         System.out.println(message);
         return message;
     }
 
+    void listAllBooks () {
+        for (Book book : (this.books)) {
+            System.out.println(book.name + "\t" + book.author + "\t" + book.editor);
+        }
+    }
+
     public static void main(String[] args) {
         BibliotecaApp bibliotecaApp = new BibliotecaApp();
         bibliotecaApp.printWelcomeMessage("Welcome to BibliotecaApp");
+        bibliotecaApp.listAllBooks();
     }
 }
--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -1,4 +1,37 @@
 package com.twu.biblioteca;
 
 public class Book {
+    String name;
+    String author;
+    String editor;
+
+    Book (String name, String author, String editor) {
+        this.setName(name);
+        this.setAuthor(author);
+        this.setEditor(editor);
+    }
+
+    void setName (String name) {
+        this.name = name;
+    }
+
+    void setAuthor (String author) {
+        this.author = author;
+    }
+
+    void setEditor (String editor) {
+        this.editor = editor;
+    }
+
+    String getName () {
+        return this.name;
+    }
+
+    String getAuthor () {
+        return this.author;
+    }
+
+    String getEditor () {
+        return this.editor;
+    }
 }
